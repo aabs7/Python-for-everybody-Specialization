308f08927cb969efefe66cfd0e9ba9ddf52376b4
aabs7 <072bex402@ioe.edu.np>
2019-01-13 11:55:03 +0545

    Regularized expression assignment completed

---
+++ CourseIII/assignment_RE.py
@@ -0,0 +1,24 @@
+#Finding Numbers in a Haystack
+
+#In this assignment you will read through and parse a file with text and numbers.
+# You will extract all the numbers in the file and compute the sum of the numbers. 
+
+import re
+
+file = input("Enter file name:")
+fHandle = open(file)
+
+ts = 0
+count = 0
+
+for line in fHandle:
+	f = re.findall('[0-9]+',line)
+	for num in f:
+		if num != []:
+			count = count + 1
+			ts = ts + int(num)
+
+print(ts)
+
+
+
