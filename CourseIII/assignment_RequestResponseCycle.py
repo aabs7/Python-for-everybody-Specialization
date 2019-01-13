c9ced439385e4c9dc6ed4a997b032c3184b8f33a
aabs7 <072bex402@ioe.edu.np>
2019-01-13 12:17:25 +0545

    assignment Network and socket completed

---
+++ CourseIII/assignment_RequestResponseCycle.py
@@ -0,0 +1,22 @@
+#You are to retrieve the following document using the HTTP protocol
+#in a way that you can examine the HTTP Response headers.
+
+#http://data.pr4e.org/intro-short.txt
+
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+mysock.connect(('data.pr4e.org', 80))
+
+url = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(url)
+
+while True:
+    data = mysock.recv(512)
+    if ( len(data) < 1 ) :
+        break
+    print(data.decode());
+
+mysock.close()
